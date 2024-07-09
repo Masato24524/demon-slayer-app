6396a2e9ee3d5289284c3c7b0e4320a3fd128715
masato <masato19729@gmail.com>
2024-07-09 16:06:18 +0900

    📝 サイトを一通り完成

--- src/App.tsx
+++ src/App.tsx
@@ -1,62 +1,159 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import axios from 'axios';
 
 interface Character {
   name: string;
   image: string;
-  id: number;
+  gallery: string[];
+  "combat style": string;
+  affiliation: string;
 }
 
-function App() {
-  const [characters, setCharacters] = useState<Character[]>([]);
-  const hasFetched = useRef(false);
+type NestedCharacter = {
+  data: Character[];
+}
 
+function App(): React.ReactElement {
+  //全てのキャラクター一覧から名前を取得
+  const [allCharacters, setAllCharacters] = useState<Character[]>([]);
   useEffect(() => {
-    const fetchData = async () => {
-      if (!hasFetched.current) {
-        try {
-          await fetchCharacters();
-          hasFetched.current = true;
-        } catch (error) {
-          console.error('Fetch error:', error);
-        }
-      }
-    }
-    fetchData();
+    const fetchAllCharacters = async () => {
+      const apiUrl = 'https://demon-slayer-api.onrender.com/v1';
+      const result = await axios.get(apiUrl);
+      console.log('API Response result:', result.data);
+      setAllCharacters(result.data);
+    };
+
+    fetchAllCharacters();
   }, []);
 
-  const fetchCharacters = async () => {
-    const apiUrl = 'https://demon-slayer-api.onrender.com/v1?limit=5';
+  //全てのキャラクター一覧から名前を取得したら、その名前でキャラクターの詳細を取得
+  const [characters, setCharacters] = useState<NestedCharacter[]>([]);
 
-    try {
-      const result = await axios.get(apiUrl);
-      console.log('API Response:', result.data);
-      setCharacters(result.data);
-    } catch (error) {
-      console.error('Error fetching characters:', error);
-    }
+  //ページネーション用
+  const displayCount = 9 //1ページあたりの表示数、APIは40件なので、9件*4ページと残り4件の計5ページ
+  const [page, setPage] = useState(1);
+  const [smallerPagination, setSmallerPagination] = useState(0);
+  const [largerPagination, setLargerPagination] = useState(displayCount);
+
+
+  // let largerPagination = 4; //初期値
+  // let smallerPagination = largerPagination - 4; //初期値
+  const handleNext = async () => {
+    console.log('hangleNext called') //デバッグ用
+    const nextPage = page + 1;
+    const newLargerPagination = nextPage*displayCount;
+    const newSmallerPagination = newLargerPagination - displayCount;
+    console.log('nextPage', nextPage);
+    console.log('newLargerPagination', newLargerPagination);
+    console.log('newSmallerPagination', newSmallerPagination);
+    setLargerPagination(newLargerPagination);
+    setSmallerPagination(newSmallerPagination);
+    // await fetchCharacters();
+    setPage(nextPage);
   }
+
+  const handlePrev = async () => {
+    console.log('hangleNext called') //デバッグ用
+    const prevPage = page - 1;
+    const newLargerPagination = prevPage*displayCount;
+    const newSmallerPagination = newLargerPagination - displayCount;
+    setLargerPagination(newLargerPagination);
+    setSmallerPagination(newSmallerPagination);
+    // await fetchCharacters();
+    setPage(prevPage);
+  }
+
+  //データのフェッチが完了するまでLoading...を表示
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchCharacters = async () => {
+      // const characterPromises = allCharacters.slice(0, allCharacters.length).map(async (character) => {
+      setLoading(true)
+      const characterPromises = allCharacters.slice(smallerPagination, largerPagination).map(async (character) => {
+        const allCharactersName = character.name.replace(" ", "_")
+        console.log(`allCharactersName`, allCharactersName);
+        const characterUrl = `https://demon-slayer-api.onrender.com/v1/${allCharactersName}`
+        const resultCharacter = await axios.get(characterUrl);
+        console.log('API Response resultCharacter:', resultCharacter.data);
+        return resultCharacter.data;
+      });
+  
+      const fetchedCharacters = await Promise.all(characterPromises);
+      console.log('Fetched Characters', fetchedCharacters); //デバッグ用
+      setCharacters(fetchedCharacters);
+      setLoading(false);
+    }
+    if (allCharacters.length > 0) {
+      fetchCharacters();
+    }
+  }, [allCharacters, page]);
+
+  console.log('characters', characters); //デバッグ用
+
   return (
     <div className="container">
+      <div className="header">
+        <div className="header-container">
+          <img src={`${process.env.PUBLIC_URL}/logo.jpg`} alt="logo" className="logo" />
+        </div>
+      </div>
       <main>
         <div className='cards-container'>
-          {characters.map((character: Character, index: number) => {
-            const encodedImageUrl = encodeURI(character.image);
-            console.log('Character:', character); // キャラクター情報をログに出力
-            console.log('Character Image:', encodedImageUrl); // 画像URLをログに出力
-            return <div className='card' key={index}>
-              <img 
-              src={character.image} 
-              alt="character"
-              className='card-image'
-              onError= {(e) => { 
-                console.error('Image load error', e);// 画像読み込みエラーをログに出力
-                e.currentTarget.src = 'logo.jpg';
-              }} 
-              />  
-            </div>;
-          })}
+          {loading ? (
+            <div>Now Loading... (take 1minute or more)</div>
+          ) : (
+           characters.map((character, index) => {
+            console.log('Rendering character', character); //デバッグ用
+            if (Array.isArray(character)) {
+              const charData = character[0];
+              const charName = charData.name.split(' ');
+              const imgSrc = page > 2 //Muzanの2回目が18件目なので、3ページ目以降は2回目の画像を表示
+              ? `${process.env.PUBLIC_URL}/${charName[0]}_profile_2.png`
+              : `${process.env.PUBLIC_URL}/${charName[0]}_Anime_Profile.png`
+              console.log('charName', `${process.env.PUBLIC_URL}/${charName[0]}_Anime_Profile.png`)
+              console.log('dummyPng', `${process.env.PUBLIC_URL}/dummy.png`)
+            return (
+              <div key={index}>
+                <div className='card'>
+                  <img  
+                  src={imgSrc}
+                  onError = {(e) => {
+                     e.currentTarget.src = `${process.env.PUBLIC_URL}/dummy.png`; 
+                    }
+                  }
+                  alt="character"
+                  className='card-image'
+                  />  
+                </div>
+                <div className='card-content'>
+                  <h3 className='card-title'> 
+                    {charData.name ? charData.name : 'No name'}
+                  </h3>
+                  <p className='card-description'>
+                    {charData["combat style"]}
+                  </p>
+                  <div className='card-footer'>
+                    <span className="affiliation">
+                      {charData.affiliation}
+                    </span>
+                  </div>
+                </div>
+              </div>
+            );
+          } else {
+            console.error('Expected character to be an array, but got:', character);
+            return null;
+          }
+           })
+          )}
+        </div>
+        <div className="pager">
+          <button disabled={page === 1} className="prev" onClick = {handlePrev}>Previous</button>
+          <span className="page-number">{`${page}`}</span>
+          <button disabled={displayCount > characters.length} className="next" onClick={handleNext}>Next</button>
         </div>
       </main>
     </div>
